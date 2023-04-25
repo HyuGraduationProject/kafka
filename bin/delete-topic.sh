83f4a55d4efdfb30f8485eb6a8e4235fa256f743
san9w9n <sw994812@hanyang.ac.kr>
2023-04-25 23:32:00 +0900

    Fix: Shell

--- bin/delete-topic.sh
+++ bin/delete-topic.sh
@@ -1,2 +1,7 @@
 echo "[delete topic]"
 bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic $1
+echo ""
+
+echo "[check topic]"
+bin/kafka-topics.sh --list --bootstrap-server localhost:9092
+echo ""
--- bin/start-kafka.sh
+++ bin/start-kafka.sh
@@ -1,17 +1,16 @@
+rm -rf /tmp/kafka-logs/*
+
 #!/bin/sh
+echo "[prometheus restart]"
+brew services restart prometheus
+sleep 1.
 
 echo "[zookeeper start]"
 bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 echo ""
 
-sleep 1.
+sleep 1.5
 echo "[kafka-start]"
-bin/kafka-server-start.sh -daemon config/server.properties
-echo ""
-
-sleep 1.
-echo "[test]"
-netstat -an | grep 2181
-lsof -i :2181
+bin/kafka-server-start.sh config/server.properties
 echo ""
 
